80b2ffb08d15c3dd909368bf4f3f543fd89fa135
Peter <maskosed@gmail.com>
2025-09-18 16:19:30 +0200

    deletet the dont needed ifs in the creatsquares function

--- javaScript.js
+++ javaScript.js
@@ -6,157 +6,84 @@ let pressedElements = [];
 document.querySelector("#large").addEventListener("click", () => {
     const container = document.getElementById('container');
     container.innerHTML = '';
-    return createSquares(large)
+    return createSquares("large")
 })
 document.querySelector("#medium").addEventListener("click", () => {
     const container = document.getElementById('container');
     container.innerHTML = '';
-    return createSquares(medium)
+    return createSquares("medium")
 })
 document.querySelector("#small").addEventListener("click", () => {
     const container = document.getElementById('container');
     container.innerHTML = '';
-    return createSquares(small)
+    return createSquares("small")
 })
 
 
-   function createSquares(format) {
-       if (format === large) {
-           for (let i = 0; i < 4096; i++) {
-               const square64 = document.createElement("div");
-               square64.classList.add('square2');
-               document.getElementById('container').appendChild(square64);
-           }
+function createSquares(format) {
+       console.log(format);
+        if (format === "large") {
 
-
-           document.addEventListener('mousedown', function (e) {
-               if (e.target.classList.contains('square2')) {
-                   isHolding = true;
-                   console.log("pisun")
-
-                   if (!pressedElements.includes(e.target)) {
-                       pressedElements.push(e.target);
-                       e.target.classList.add("pressed")
-                       e.target.style.backgroundColor = 'red';
-                   }
-               }
-           });
-           document.addEventListener('mouseover', function (e) {
-               if (isHolding && e.target.classList.contains("square2")) {
-                   if (!pressedElements.includes(e.target)) {
-                       pressedElements.push(e.target);
-                       e.target.classList.add("pressed")
-                       e.target.style.backgroundColor = 'red';
-                   }
-               }
-           })
-
-           document.addEventListener("mouseup", function (e) {
-               isHolding = false
-           })
-           document.addEventListener("click", function (e ){
-               if (e.target.id === "buttonr") {
-                   // твой код
-                   pressedElements.forEach(element => {
-                       element.classList.remove('pressed');
-                       element.style.backgroundColor= "";
-                   });
-                   pressedElements = [];
-                   console.log("adlskdlasd")
-               }
-           })
+            buildSquares(4096, "square2");
        }
-       if (format === medium) {
-           for (let i = 0; i < 1024; i++) {
-               const square64 = document.createElement("div");
-               square64.classList.add('square1');
-               document.getElementById('container').appendChild(square64);
-           }
-
-
-           document.addEventListener('mousedown', function (e) {
-               if (e.target.classList.contains('square1')) {
-                   isHolding = true;
-                   console.log("pisun")
-
-                   if (!pressedElements.includes(e.target)) {
-                       pressedElements.push(e.target);
-                       e.target.classList.add("pressed")
-                       e.target.style.backgroundColor = 'red';
-                   }
-               }
-           });
-           document.addEventListener('mouseover', function (e) {
-               if (isHolding && e.target.classList.contains("square1")) {
-                   if (!pressedElements.includes(e.target)) {
-                       pressedElements.push(e.target);
-                       e.target.classList.add("pressed")
-                       e.target.style.backgroundColor = 'red';
-                   }
-               }
-           })
-
-           document.addEventListener("mouseup", function (e) {
-               isHolding = false
-           })
-           document.addEventListener("click", function (e ){
-               if (e.target.id === "buttonr") {
-                   pressedElements.forEach(element => {
-                       element.classList.remove('pressed');
-                       element.style.backgroundColor= "";
-                   });
-                   pressedElements = [];
-                   console.log("adlskdlasd")
-               }
-           })
+       if (format === "medium") {
+           buildSquares(1024, "square1");
        }
-       if (format === small) {
-           for (let i = 0; i < 256; i++) {
-               const square64 = document.createElement("div");
-               square64.classList.add('square');
-               document.getElementById('container').appendChild(square64);
-           }
-
-
-           document.addEventListener('mousedown', function (e) {
-               if (e.target.classList.contains('square')) {
-                   isHolding = true;
-                   console.log("pisun")
-
-                   if (!pressedElements.includes(e.target)) {
-                       pressedElements.push(e.target);
-                       e.target.classList.add("pressed")
-                       e.target.style.backgroundColor = 'red';
-                   }
-               }
-           });
-           document.addEventListener('mouseover', function (e) {
-               if (isHolding && e.target.classList.contains("square")) {
-                   if (!pressedElements.includes(e.target)) {
-                       pressedElements.push(e.target);
-                       e.target.classList.add("pressed")
-                       e.target.style.backgroundColor = 'red';
-                   }
-               }
-           })
-
-           document.addEventListener("mouseup", function (e) {
-               isHolding = false
-           })
-           document.addEventListener("click", function (e ){
-               if (e.target.id === "buttonr") {
-                   pressedElements.forEach(element => {
-                       element.classList.remove('pressed');
-                       element.style.backgroundColor= "";
-                   });
-                   pressedElements = [];
-                   console.log("adlskdlasd")
-               }
-           })
+       if (format === "small") {
+           buildSquares(256, "square");
        }
 }
 
 
+
+
+
+function buildSquares(pixels, squaresNumber) {
+    console.log(pixels, squaresNumber);
+    for (let i = 0; i < pixels; i++) {
+        const square64 = document.createElement("div");
+        square64.classList.add(squaresNumber);
+        document.getElementById('container').appendChild(square64);
+    }
+
+
+    document.addEventListener('mousedown', function (e) {
+        if (e.target.classList.contains(squaresNumber)) {
+            isHolding = true;
+            console.log("pisun")
+
+            if (!pressedElements.includes(e.target)) {
+                pressedElements.push(e.target);
+                e.target.classList.add("pressed")
+                e.target.style.backgroundColor = 'red';
+            }
+        }
+    });
+    document.addEventListener('mouseover', function (e) {
+        if (isHolding && e.target.classList.contains(squaresNumber)) {
+            if (!pressedElements.includes(e.target)) {
+                pressedElements.push(e.target);
+                e.target.classList.add("pressed")
+                e.target.style.backgroundColor = 'red';
+            }
+        }
+    })
+
+    document.addEventListener("mouseup", function (e) {
+        isHolding = false
+    })
+    document.addEventListener("click", function (e) {
+        if (e.target.id === "buttonr") {
+            // твой код
+            pressedElements.forEach(element => {
+                element.classList.remove('pressed');
+                element.style.backgroundColor = "";
+            });
+            pressedElements = [];
+            console.log("adlskdlasd")
+        }
+    })
+}
 
 
 
