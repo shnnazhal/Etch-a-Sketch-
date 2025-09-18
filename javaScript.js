77307b8edf477d6cc9ef2bf9887d873f8c5303a7
Peter <maskosed@gmail.com>
2025-09-18 11:34:28 +0200

    added the logic. Yet you can draw on the squads and alsaw resset the color

--- javaScript.js
+++ javaScript.js
@@ -1,7 +1,52 @@
-for (let i = 0; i < 1024; i++) {
+
+
+let isHolding = false;
+let pressedElements = []
+
+for (let i = 0; i < 4096; i++) {
     const square = document.createElement("div");
     square.classList.add('square');
     document.getElementById('container').appendChild(square);
 }
 
-console.log("4 squares created!");
+
+document.addEventListener('mousedown', function (e) {
+    if (e.target.classList.contains('square')) {
+        isHolding = true;
+        console.log("pisun")
+
+        if (!pressedElements.includes(e.target)) {
+            pressedElements.push(e.target);
+            e.target.classList.add("pressed")
+            e.target.style.backgroundColor = 'red';
+        }
+    }
+});
+document.addEventListener('mouseover', function (e) {
+    if (isHolding && e.target.classList.contains("square")) {
+        if (!pressedElements.includes(e.target)) {
+            pressedElements.push(e.target);
+            e.target.classList.add("pressed")
+            e.target.style.backgroundColor = 'red';
+        }
+    }
+})
+
+document.addEventListener("mouseup", function (e) {
+    isHolding = false
+})
+document.addEventListener("click", function (e ){
+    if (e.target.id === "buttonr") {
+        // твой код
+        pressedElements.forEach(element => {
+            element.classList.remove('pressed');
+            element.style.backgroundColor= "";
+        });
+        pressedElements = [];
+        console.log("adlskdlasd")
+    }
+})
+
+
+
+//My problem is that the
